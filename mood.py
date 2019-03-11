88fe698f6462bc3ce652c4e9f2c79c2517bf184f
hrvoje <hrvoje.viskovic>
2019-03-11 22:17:44 +0100

    prvi python programcic

---
+++ mood.py
@@ -0,0 +1,17 @@
+ime = input("Kako se zoveš? ")
+raspolozenje = input ("Pozdrav "+ime+", kako si? ")
+
+if raspolozenje == "dobro":
+    print ("Baš mi je drago da si dobro!")
+elif raspolozenje == "pospano":
+    print ("A takvo je vrijeme")
+elif raspolozenje == "nervozno":
+    print ("Opusti se i uživaj")
+elif raspolozenje == "tužan":
+    print("Razvedri se")
+elif raspolozenje == "sretan":
+    print("Lupi dlanom ti o dlan")
+elif raspolozenje == "bolesno":
+    print("Tablete će ti pomoći")
+else:
+    print ("Ne znam što bih ti rekao")
